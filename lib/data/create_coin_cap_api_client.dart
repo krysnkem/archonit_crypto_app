5e2fd7762a2b86b94d54a0e57fc4c940df178aa5
MPos <krysnkem@gmail.com>
2025-07-23 23:48:39 +0100

    implement interceptor and client creation method

---
+++ lib/data/create_coin_cap_api_client.dart
@@ -0,0 +1,10 @@
+import 'package:archonit_crypto_app/data/api/coincap_api.dart';
+import 'package:archonit_crypto_app/data/interceptors/api_key_interceptor.dart';
+import 'package:dio/dio.dart';
+
+CoinCapApiClient createCoinCapApiClient(String apiKey) {
+  final dio = Dio();
+  dio.interceptors.add(ApiKeyInterceptor(apiKey));
+  dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
+  return CoinCapApiClient(dio);
+}
