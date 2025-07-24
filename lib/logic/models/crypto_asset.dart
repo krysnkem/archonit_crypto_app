b56a4c5b20985c2d763f47aac24f56e809019378
MPos <krysnkem@gmail.com>
2025-07-24 01:40:20 +0100

    add crypto list state

---
+++ lib/logic/models/crypto_asset.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CryptoAsset {
+  final Color color;
+  final String id;
+  final String name;
+  final String symbol;
+
+  CryptoAsset({
+    required this.color,
+    required this.id,
+    required this.name,
+    required this.symbol,
+  });
+}
---
+++ lib/logic/notifiers/crypto_list_state.dart
@@ -0,0 +1,34 @@
+import 'package:archonit_crypto_app/logic/models/crypto_asset.dart';
+import 'package:equatable/equatable.dart';
+
+sealed class CryptoListState extends Equatable {
+  const CryptoListState({this.cryptoList = const []});
+  final List<CryptoAsset> cryptoList;
+
+  @override
+  List<Object?> get props => [cryptoList];
+}
+
+class CryptoListInitial extends CryptoListState {}
+
+class CryptoListLoading extends CryptoListState {}
+
+class CryptoListLoaded extends CryptoListState {
+  const CryptoListLoaded({required super.cryptoList});
+  @override
+  List<Object?> get props => [cryptoList];
+}
+
+class CryptoListLoadingMore extends CryptoListState {
+  const CryptoListLoadingMore({required super.cryptoList});
+  @override
+  List<Object?> get props => [cryptoList];
+}
+
+class CryptoListError extends CryptoListState {
+  final String message;
+  const CryptoListError({required this.message, super.cryptoList});
+
+  @override
+  List<Object?> get props => [message, cryptoList];
+}
