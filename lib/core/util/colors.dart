f78c76bebbc8d72a526d5383b8a160ae51022749
MPos <krysnkem@gmail.com>
2025-07-24 09:01:42 +0100

    set up styling for text

---
+++ lib/core/util/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/services.dart';
+
+const textBlack = Color(0xFF17171A);
---
+++ lib/core/util/text_styles.dart
@@ -0,0 +1,8 @@
+import 'package:archonit_crypto_app/core/util/colors.dart';
+import 'package:flutter/widgets.dart';
+
+const TextStyle sfProText17600TextBlack = TextStyle(
+  fontFamily: 'SF Pro Display',
+  fontSize: 17.0,
+  color: textBlack,
+);
