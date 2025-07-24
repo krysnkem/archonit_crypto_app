454b031b8f18a4b2612a59719576e38063577d4b
MPos <krysnkem@gmail.com>
2025-07-24 14:04:21 +0100

    refactor color generator comments

--- lib/core/util/color_generator.dart
+++ lib/core/util/color_generator.dart
@@ -7,9 +7,8 @@ class ColorGenerator {
   static final Random _random = Random();
   static Color generateColor() {
     // Generate from full RGB spectrum (16,777,216 colors)
-    // Using semi-transparent colors for better visual appeal
     return Color.fromARGB(
-      180, // Semi-transparent for better readability
+      180,
       _random.nextInt(256), // Red: 0-255
       _random.nextInt(256), // Green: 0-255
       _random.nextInt(256), // Blue: 0-255
